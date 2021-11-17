0ee618aaed7d4933e5cf481cc446661639e8a7a9
Tom Groot <4732713+Tomgroot@users.noreply.github.com>
2021-11-17 18:08:38 +0100

    feat: added authorize request for CSE support

---
+++ src/Message/Response/AuthorizeResponse.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Omnipay\Paynl\Message\Response;
+
+
+class AuthorizeResponse extends AbstractPaynlResponse
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function isSuccessful()
+    {
+        return isset($this->data['request']['result']) && $this->data['request']['result'] == 1;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTransactionReference()
+    {
+        return isset($this->data['transaction']['transactionId']) ? $this->data['paymentDetails']['transactionId'] : null;
+    }
+
+    /**
+     * @return mixed|string|null
+     */
+    public function getCode()
+    {
+        return isset($this->data['request']['errorId']) ? $this->data['request']['errorId'] : null;
+    }
+
+}
